d247c2a863c20e03e049c2fa8c5b4ae3b463ac64
Pluttark <59135750+levuper@users.noreply.github.com>
2020-06-16 17:00:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -38,33 +38,96 @@ let phrases = [
     {
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         image: 'https://randomwordgenerator.com/img/picture-generator/54e3dc4a425aaf14f1dc8460962e33791c3ad6e04e507440752f72d79745c5_640.jpg'
-    },{
+    }, {
         text: 'Благородные стремления не спасут: героям были возданы соответствующие почести',
         image: 'https://randomwordgenerator.com/img/picture-generator/55e2dc4a4b52a414f1dc8460962e33791c3ad6e04e5074417d2d73dc9444c7_640.jpg'
-    },{
+    }, {
         text: 'Не забудьте выдать мне доступ ко всей переписке в настройках беседы, а то ',
         image: 'https://randomwordgenerator.com/img/picture-generator/54e9dd444e55aa14f1dc8460962e33791c3ad6e04e5074417d2d73d39f4ec1_640.jpg'
-    },{
+    }, {
         text: 'MOLOKO С НОЖАМИ',
-        image: 'https://randomwordgenerator.com/img/picture-generator/54e8d74b4b56aa14f1dc8460962e33791c3ad6e04e507440752f72d69e4ec0_640.jpg'
-    },{
+        image: 'https://randomwordgenerator.com/img/picture-generator/57e6d2474e52a814f1dc8460962e33791c3ad6e04e507441722a72dd9e4bc6_640.jpg'
+    }, {
         text: 'Я съел ноги у которого подряд',
         image: 'https://randomwordgenerator.com/img/picture-generator/52e9d4414e57b10ff3d8992cc12c30771037dbf85254794075277bd3914e_640.jpg'
-    },{
+    }, {
         text: 'Даже евгений понасенков теперь у меня, но не начинай этот бот говорит сел любитель постиронии к японии, прется от него',
         image: 'https://randomwordgenerator.com/img/picture-generator/5ee4d1404a4faa0df7c5d57bc32f3e7b1d3ac3e45659784f70267edc94_640.jpg'
-    },{
+    }, {
         text: 'сто используете эту команду. Подождите немного и попробуйте сно',
         image: 'https://randomwordgenerator.com/img/picture-generator/locomotive-616267_640.jpg'
-    },{
+    }, {
         text: 'ЧЕРЕЗ ЧАС ИЛИ ПОЛЧАСА',
         image: 'https://randomwordgenerator.com/img/picture-generator/55e8dd4a4255b10ff3d8992cc12c30771037dbf85254794e73277bd7954a_640.jpg'
-    },{
+    }, {
         text: 'Я мы кабан я хочу новую аватарку',
         image: 'https://randomwordgenerator.com/img/picture-generator/52e1d64a435aae14f1dc8460962e33791c3ad6e04e507441722a72dd904ac1_640.jpg'
-    },{
+    }, {
         text: 'ДОСТУП К ПОЛУНОЧИ БЫЛО ИДТИ В И БРОСИТЬ В БЮДЖЕТ НЕ СПОРЮ',
         image: 'https://randomwordgenerator.com/img/picture-generator/53e0d0464a57a914f1dc8460962e33791c3ad6e04e5074417d2d73d3914fc4_640.jpg'
+    },
+    {
+        text: 'Неверный аргумент команды. Используйте: s g <число о',
+        image: 'https://randomwordgenerator.com/img/picture-generator/51e1d2454256b10ff3d8992cc12c30771037dbf85254784973267ddd914b_640.jpg'
+    },
+    {
+        text: 'мечи , броня чекни в се в 6 лет пошел? просто до летом ну вау но чета не понимаю как с дробовиками делать удобнее потому что Аргон ',
+        image: 'https://randomwordgenerator.com/img/picture-generator/57e1d1404e5bb10ff3d8992cc12c30771037dbf852547941742679d0924f_640.jpg'
+    },
+    {
+        text: 'количество сообщений фывапролджвапролджфывапролджэт?',
+        image: 'https://randomwordgenerator.com/img/picture-generator/5ee1d2424f4faa0df7c5d57bc32f3e7b1d3ac3e4565979487c2973d095_640.jpg'
+    },
+    {
+        text: 'это ты щас усложняешь ну просто не хочется туда сюда кидать да перекидывать bruh просто скинуть в лс Ка уважаемый',
+        image: 'https://randomwordgenerator.com/img/picture-generator/54e3d7404956a814f1dc8460962e33791c3ad6e04e50744172287ad2924fc4_640.jpg'
+    },
+    {
+        text: 'И купи коробку конфет, и положи туда листочек с любой надписью',
+        image: 'https://randomwordgenerator.com/img/picture-generator/hand-2571553_640.jpg'
+    },
+    {
+        text: 'напишите в телеге что тебе разрешают ночь',
+        image: 'https://randomwordgenerator.com/img/picture-generator/5ee7d0424c57b10ff3d8992cc12c30771037dbf85254794e702673dd9e4b_640.jpg'
+    }, {
+        text: 'Ро не паспортом занимаюсь, дадам почку',
+        image: 'https://randomwordgenerator.com/img/picture-generator/53e2dc444e57ab14f1dc8460962e33791c3ad6e04e507440772d7cdd934bc2_640.jpg'
+    }, {
+        text: 'Это потому что если не будет анонимности то возможно что голосовать будешь, то она просто тобой попользуется и выбросит',
+        image: 'https://randomwordgenerator.com/img/picture-generator/54e5d2444253af14f1dc8460962e33791c3ad6e04e50744172287ad1954ec7_640.jpg'
+    }, {
+        text: 'ХАХА АХАХАХАХА АХАХАХАХА АХАМеня, разлогниься',
+        image: 'https://randomwordgenerator.com/img/picture-generator/5fe8d1434355b10ff3d8992cc12c30771037dbf85254794e732a7cd69644_640.jpg'
+    }, {
+        text: 'А если по нормальному я спать всем спокойной ночи',
+        image: 'https://randomwordgenerator.com/img/picture-generator/54e0d6444e53a414f1dc8460962e33791c3ad6e04e5074417c2d78d39444c4_640.jpg'
+    }, {
+        text: 'Александр творил хрень, за которую должен был быть опыт',
+        image: 'https://randomwordgenerator.com/img/picture-generator/5ee0d44b4854b10ff3d8992cc12c30771037dbf85254794e722e7edd974c_640.jpg'
+    }, {
+        text: 'must.learn=true must.write=true must.make.memes=false everybody.can.use.comma',
+        image: 'https://randomwordgenerator.com/img/picture-generator/57e5d3464f5bab14f1dc8460962e33791c3ad6e04e50744172297cdc904dcc_640.jpg'
+    }, {
+        text: 'подобный хахахаах ахахахахахослушать в урет возвыситься достойны лишь те кто побывал на дне ментатью на вики ржакое-нибудь карате запишется',
+        image: 'https://randomwordgenerator.com/img/picture-generator/boards-2040575_640.jpg'
+    },{
+        text: 'блин а если я пол машину брлшусь че будет че я понимаю, что буду рыдать сегодня пол ночи спасибо всем, кто поддерживал меня в моей жизни, спасибо',
+        image: 'https://randomwordgenerator.com/img/picture-generator/57e6d34b4b54ab14f1dc8460962e33791c3ad6e04e507441722a72d3964ec2_640.jpg'
+    },{
+        text: 'Сложно сказать, почему парад бытовой техники оказался началом великой войны',
+        image: 'https://randomwordgenerator.com/img/picture-generator/57e7dd414c53a514f1dc8460962e33791c3ad6e04e507440752972d3924bc6_640.jpg'
+    },{
+        text: 'Смешно, но зима близко',
+        image: 'https://randomwordgenerator.com/img/picture-generator/55e1d5474957a914f1dc8460962e33791c3ad6e04e507440752f72d79f4fc4_640.jpg'
+    },{
+        text: 'по типу yves rocher',
+        image: 'https://randomwordgenerator.com/img/picture-generator/55e6d74a4b53a514f1dc8460962e33791c3ad6e04e507440742e7dd59245c0_640.jpg'
+    },{
+        text: 'он меня не слушаляйцу',
+        image: 'https://randomwordgenerator.com/img/picture-generator/52e1d5424b56aa14f1dc8460962e33791c3ad6e04e50744074267bd69149c7_640.jpg'
+    },{
+        text: 'высшие демоны это петушары ',
+        image: 'https://randomwordgenerator.com/img/picture-generator/57e0d040495ba814f1dc8460962e33791c3ad6e04e50744074267bd69549c2_640.jpg'
     },
 ];
 
